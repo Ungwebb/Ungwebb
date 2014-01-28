19351092165ee841f7bc302bd65b937b96ba27a3
Linus Odenring <linus@odenring.se>
2014-01-28 13:23:23 +0000

    Adding project

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ung Webb</title>
+ <meta charset="iso-8859-1">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+  <link href="images/favicon.ico" rel="icon" type="image/x-icon" />
+  <link rel="canonical"  href="http://ungwebb.com"/>
+  <meta name="description" content="Ung webb är en ideell förening med syfte att stödja ungdomar i deras inlärning av webbutveckling samt apputveckling!">
+  <meta name="keywords" content="ung,webb,sverige,vi unga,webbutveckling,förening,vi unga">
+  <meta http-equiv="X-UA-Compatible" content="IE=8"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+
+  <meta property="og:title" content="Ung Webb"/>
+  <meta property="og:site_name" content="Ung Webb"/>
+  <meta property="og:type"   content="website" />
+  <meta property="og:url" content="http://ungwebb.com"/>
+  <meta property="og:description" content="Ung webb är en ideell förening med syfte att stödja ungdomar i deras inlärning av webbutveckling samt apputveckling!"/>
+  <meta property="og:image"  content="http://ungwebb.com/images/fb.png" /> 
+
+  <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700|Open+Sans:300italic,400,300,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="css/semantic.min.css">
+  <link rel="stylesheet" type="text/css" href="css/master.css">
+
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.js"></script>
+  <script src="javascript/semantic.min.js"></script>
+  <script src="javascript/main.js"></script>
+  <script>
+  $( document ).ready(function() {
+  $('.logo')
+  .transition('scale in')
+;
+});
+</script>
+</head>
+<body>
+<div class="splash container">
+  <div class="ui one column grid">
+  <div class="column">
+<img class="logo" src="images/logo.png" style="margin-left: 10px;"/>
+<h2>Välkommen!</h2>
+<p>Ung webb är en ideell förening med syfte att stödja ungdomar i deras utveckling inom webbutveckling samt apputveckling!</p>
+<center style="margin-left: 40px;"><!-- AddThis Button BEGIN -->
+<div class="addthis_toolbox addthis_default_style ">
+<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
+<a class="addthis_button_tweet"></a>
+<a class="addthis_button_pinterest_pinit" pi:pinit:layout="horizontal"></a>
+<a class="addthis_counter addthis_pill_style"></a>
+</div>
+<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-52a3853830375704"></script>
+<!-- AddThis Button END -->
+</center>
+<div class="ui horizontal icon divider">
+  <i class="circular heart icon"></i>
+</div>
+<center>
+	<a href="http://digitalocean.com" target="_BLANK"><img src="images/digitalocean.png" data-position="bottom center" class="company" data-title="Digitalocean" data-content="Digitalocean är en VPS leverantör med säte i USA!"/></a>
+	<a href="http://wiki.nginx.org/Main" target="_BLANK"><img src="images/nginx.png" class="company" data-position="bottom center" class="fade company" data-title="Nginx" data-content="Nginx är en snabb och stabil webbserverprogramvara!" /></a>
+	<a href="http://viunga.se" target="_BLANK"><img src="images/viunga.png" class="company" data-position="bottom center" class="fade company" data-title="Förbundet Vi Unga" data-content='Förbundet Vi Unga är en organsaioner som arbetar med barn och ungdomar!' /></a>
+</center>
+
+</div>
+</div>
+</div>
+<script>
+$( document ).ready(function() {
+$('.company')
+  .popup({
+    on: 'hover'
+  })
+;
+});
+</script>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=147475602116000";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+</body>
+</html>
